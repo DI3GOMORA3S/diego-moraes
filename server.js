606cf1341210f0f1cf977882fb3905ecc2db9ffd
DI3GOMORA3S <116921992+DI3GOMORA3S@users.noreply.github.com>
2025-07-28 20:10:50 -0300

    Sections added

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,13 +5,11 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Obtener __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Servir archivos estáticos desde /public
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, '0.0.0.0',() => {
-  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
